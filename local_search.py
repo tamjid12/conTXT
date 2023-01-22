f83d671426692b5272d5ba8a8a29945e570511de
Akid Tamjid Rahman <90531515+tamjid12@users.noreply.github.com>
2023-01-22 07:31:49 -0500

    Added Comments

--- local_search.py
+++ local_search.py
@@ -5,13 +5,16 @@ import os
 # relevant to the keyword saving time and energy and working efficiently.
 
 def search_files(directory, keywords):
+    # Initialize count array with 0 for each keyword
     count = [0 for i in range(len(keywords))]
     file_count = 0
+    # Iterate through all files in the directory
     for root, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
             with open(file_path, "r") as f:
                 lines = f.readlines()
+                # Initialize has_keyword array with False for each keyword
                 has_keyword = [False for i in range(len(keywords))]
                 for line in lines:
                     for i, keyword in enumerate(keywords):
